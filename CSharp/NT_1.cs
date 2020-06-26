6bb9f6d1870bf54489db4807044f7c29368ea411
JuicyPark <bassoff@naver.com>
2020-06-26 22:39:20 +0900

    0626 c#

---
+++ CSharp/NT_1.cs
@@ -0,0 +1,20 @@
+﻿class NT_1
+{
+    public int solution(int[] arr)
+    {
+        int answer = 0;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (FindTeam(arr, i))
+                answer++;
+        }
+        return answer / 3;
+    }
+
+    bool FindTeam(int[] arr, int index)
+    {
+        return index + 1 == arr[arr[arr[index] - 1] - 1];
+    }
+
+}
